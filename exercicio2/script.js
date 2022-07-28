eaa873a29ad758fc6cdcc50b43296d3ef3818100
tcariri <thais.cariri2016@gmail.com>
2022-07-27 22:43:14 -0400

    exercicio-pratico 010

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayNumbers = [45,65,32,12,2,8,9]
+const arrayStrings = ['segunda','terça','quarta','quinta']
+const arrayMisto = [45,'segunda',true]
+
+console.log('arrayNumbers',arrayNumbers.length)
+console.log('arrayStrings',arrayStrings.length)
+console.log('arrayMisto',arrayMisto.length)
+console.log('imprima',arrayNumbers[0],arrayStrings[1],arrayMisto[2])
+console.log('inclusão 1',arrayNumbers.includes(45))
+console.log('inclusão 2',arrayMisto.includes(32))
